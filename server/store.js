83396c353527fb1ca1cbea99064149997105877e
Vinay <vbvyas@gmail.com>
2014-10-03 22:47:07 -0700

    1st commit

---
+++ server/store.js
@@ -0,0 +1,31 @@
+var liveDbMongo = require('livedb-mongo');
+var coffeeify = require('coffeeify');
+
+module.exports = store;
+
+function store(derby, publicDir) {
+
+  derby.use(require('racer-bundle'));
+
+  var redisClient = require('redis').createClient();
+  redisClient.select(process.env.REDIS_DB);
+
+  var store = derby.createStore({
+    db: liveDbMongo(process.env.MONGO_URL + '?auto_reconnect', {safe: true}),
+    redis: redisClient
+  });
+
+  store.on('bundle', function(browserify) {
+
+    browserify.transform({global: true}, coffeeify);
+
+    var pack = browserify.pack;
+    browserify.pack = function(opts) {
+      var detectTransform = opts.globalTransform.shift();
+      opts.globalTransform.push(detectTransform);
+      return pack.apply(this, arguments);
+    };
+  });
+
+  return store;
+}
